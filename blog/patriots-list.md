91a2631aeafd91f8f245f6e00e914fd208819ade
Jaroslaw Cichon <dev@iana.io>
2024-06-24 08:10:17 +0000

    clear tmpl

---
+++ blog/patriots-list.md
@@ -0,0 +1,35 @@
+---
+slug: patriots 
+title: Patriot's list
+author: Cichy
+author_title: Maintainer of IanaIO 
+author_url: https://github.com/cichy
+author_image_url: https://avatars.githubusercontent.com/u/443253?v=4
+tags: [release]
+---
+
+
+
+
+## What's mew
+
+txt
+
+### New
+
+txt
+
+### Features
+
+txt
+
+## Notable mention from last post
+
+### `history`
+
+txt
+
+## Looking for contributors
+
+txt
+
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,9 @@ const config = {
         src: 'img/proudamericaus.png',
       },
       items: [
-        {to: '/blog', label: 'Blog', position: 'left'},
+        {to: '/blog', label: 'Blog', position: 'left'}, 
+	{to: '/shop', label: 'Shop', position: 'left'},
+        {to: '/patriots', label: 'Patriots', position: 'left'}
       ],
     },
     footer: {
@@ -89,7 +91,7 @@ const config = {
         width: 27.83,
         height: 32,
       },
-      copyright: `Copyright © ${new Date().getFullYear()} Proud America - The United States of America, Inc.`, // You can also put own HTML here
+      copyright: `Copyright © ${new Date().getFullYear()} Proud America ★ American Made ★ American Owned, Inc.`, // You can also put own HTML here
     },
     prism: {
       theme: lightCodeTheme,
